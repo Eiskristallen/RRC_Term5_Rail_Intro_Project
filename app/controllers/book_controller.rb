4759a834eda8fafe3e47630fe914d16081197dd4
Eiskristallen <qhuangb47@gmail.com>
2021-10-31 16:54:48 -0500

    feat:add search function

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -5,4 +5,8 @@ class BookController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @books = Book.where("title LIKE ?",wildcard_search)
+  end
 end
